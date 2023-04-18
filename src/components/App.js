8d697be2214ceba7ecab6a0e3749d72dad58bc15
Peter Nordel <peternordel@gmail.com>
2023-04-18 17:33:00 -0600

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,19 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">About Section</div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
